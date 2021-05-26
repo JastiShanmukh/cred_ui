a874fa351f944cdca07e20f97f857fd43ed0c515
Jasti Shanmukha Sai <shanmukhasai.jasti@gmail.com>
2021-05-27 00:34:58 +0530

    Final Output

--- lib/second.dart
+++ lib/second.dart
@@ -24,34 +24,21 @@ class _SecondState extends State<Second> {
             ),
             Container(
               width: width * 0.9,
-              child: Text(
-                "give us your \nmobile number ",
-                style: TextStyles.style2,
+              child: Text("give us your \nmobile number ",style: TextStyles.style2),
               ),
-            ),
-            SizedBox(
-              height: height * 0.01,
-            ),
+            SizedBox(height: height * 0.01),
             Container(
               width: width * 0.9,
-              child: Text(
-                "to apply, we need your mobile\nnumber linked to your credit card",
+              child: Text("to apply, we need your mobile\nnumber linked to your credit card",
                 style: TextStyles.style3,
               ),
             ),
-            SizedBox(
-              height: height * 0.04,
-            ),
+            SizedBox(height: height * 0.04),
             Container(
               width: width * 0.9,
-              child: Text(
-                "9999999999",
-                style: TextStyles.style4,
-              ),
+              child: Text("9999999999",style: TextStyles.style4),
             ),
-            SizedBox(
-              height: height * 0.25,
-            ),
+            SizedBox(height: height * 0.25),
             Container(
               color: backgroundColor,
               width: width,
@@ -71,18 +58,54 @@ class _SecondState extends State<Second> {
                         height: height * 0.05,
                       ),
                     ),
-                    SizedBox(height: height*0.02,),
-                    Container(width: width*0.9,child: Text("you agree to allow CRED to check your credit information\nwith RBI approved credit bureaus.",style: TextStyles.style5,),),
-                      SizedBox(height: height*0.04),
-                      Container(width: width*0.9,child: Text("we need to check if you are a credit card holder and are\nabove our accepted credit score threshold. it will not \nimpact your credit score",style: TextStyles.style5,),),
-                      SizedBox(height: height*0.04),
-                     Padding(
-                       padding: const EdgeInsets.only(left:20.0),
-                       child: Align(alignment: Alignment.topLeft, child: InkWell(onTap: (){Navigator.pop(context,MaterialPageRoute(builder: (context)=>Home()));},
-                         child: Container(width: width*0.5,height: height*0.065,decoration: BoxDecoration(boxShadow: [BoxShadow(color: Colors.white30,offset: Offset(-3.0,-3.0),blurRadius: 2)],
-                           borderRadius: BorderRadius.circular(20.0),color: backgroundColor,),child: Center(child: Text("Agree & Continue",style: TextStyles.style6),),),
-                       )),
-                     )
+                    SizedBox(height: height * 0.02),
+                    Container(
+                      width: width * 0.9,
+                      child: Text(
+                        "you agree to allow CRED to check your credit information\nwith RBI approved credit bureaus.",
+                        style: TextStyles.style5,
+                      ),
+                    ),
+                    SizedBox(height: height * 0.04),
+                    Container(
+                      width: width * 0.9,
+                      child: Text(
+                        "we need to check if you are a credit card holder and are\nabove our accepted credit score threshold. it will not \nimpact your credit score",
+                        style: TextStyles.style5,
+                      ),
+                    ),
+                    SizedBox(height: height * 0.04),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Align(
+                          alignment: Alignment.topLeft,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pop(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Home()));
+                            },
+                            child: Container(
+                              width: width * 0.5,
+                              height: height * 0.065,
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.white30,
+                                      offset: Offset(-3.0, -3.0),
+                                      blurRadius: 2)
+                                ],
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: backgroundColor,
+                              ),
+                              child: Center(
+                                child: Text("Agree & Continue",
+                                    style: TextStyles.style6),
+                              ),
+                            ),
+                          )),
+                    )
                   ],
                 ),
               ),
